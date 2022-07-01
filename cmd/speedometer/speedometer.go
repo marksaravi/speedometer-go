406c563c226f4457163f4799ac2476413d9d02a4
Mark Saravi <mark.saravi@gmail.com>
2022-07-01 22:30:15 +1000

    update info

--- cmd/speedometer/speedometer.go
+++ cmd/speedometer/speedometer.go
@@ -20,16 +20,32 @@ func main() {
 }
 
 func process(lcd lcdDisplay, input gpio.PinIn) {
-	lcd.Update(17.3, 847.45, time.Second*4325)
-	ts := time.Now()
+	const PERIMETER float64 = 2.2
+	const PULSE_PER_PERIMETER = 8
+	const DIST_PER_PULSE = PERIMETER / PULSE_PER_PERIMETER
+
+	start := time.Now()
 	var counter int = 0
+	ts := time.Now()
+	prevTime := time.Now()
+	currTime := time.Now()
 	for {
+		time.Sleep(time.Millisecond * 10)
 		input.Read()
 		counter++
+		currTime = time.Now()
+		dt := currTime.Sub(prevTime)
+		prevTime = currTime
+		speed := DIST_PER_PULSE / dt.Seconds() * 1000 / 3600
+		dur := time.Since(start)
+		distance := DIST_PER_PULSE * float64(counter)
+
 		if time.Since(ts) >= time.Second {
 			fmt.Println(counter)
-			counter = 0
 			ts = time.Now()
+			func() {
+				lcd.Update(speed, distance, dur)
+			}()
 		}
 	}
 }
