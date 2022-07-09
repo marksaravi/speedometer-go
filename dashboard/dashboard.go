4ba8e50d9a8079bb1ee0d42447e3bd404d0240fa
Mark Saravi <mark.saravi@gmail.com>
2022-07-09 21:47:04 +1000

    clear zones

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -8,15 +8,26 @@ import (
 	"github.com/marksaravi/fonts-go/fonts"
 )
 
+type clearZone struct {
+	x1, y1, x2, y2 int
+}
 type dashboardDisplay struct {
-	display display.RGBDisplay
-	theme   Theme
+	display    display.RGBDisplay
+	theme      Theme
+	clearZones []clearZone
 }
 
 func NewDashboardDisplay(display display.RGBDisplay) *dashboardDisplay {
 	return &dashboardDisplay{
 		display: display,
 		theme:   DarkTheme,
+		clearZones: []clearZone{
+			{0, 0, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0},
+			{0, 0, 0, 0},
+		},
 	}
 }
 
@@ -27,52 +38,52 @@ func (d *dashboardDisplay) Initialise() {
 func (d *dashboardDisplay) UpdateSpeed(speed float64) {
 	x := DATA_X
 	y := SPEED_DATA_LINE_Y
-	d.printDigits(fmt.Sprintf("%3.1f", speed), SPEED_DATA_FONT, d.theme.SpeedDataColor, x, y)
+	d.printDigits(fmt.Sprintf("%3.1f", speed), SPEED_DATA_FONT, d.theme.SpeedDataColor, x, y, 0)
 }
 
 func (d *dashboardDisplay) UpdateDistance(distance float64) {
 	x := DATA_X
 	y := DISTANCE_DATA_LINE_Y
-	d.printDigits(fmt.Sprintf("%4.2f", distance/1000), DISTANCE_DATA_FONT, d.theme.DistanceDataColor, x, y)
+	d.printDigits(fmt.Sprintf("%4.2f", distance/1000), DISTANCE_DATA_FONT, d.theme.DistanceDataColor, x, y, 1)
 }
 
 func (d *dashboardDisplay) UpdateDuration(t int, change int) {
 	digits := fmt.Sprintf("%02d", t)
 	x := DATA_X
 	y := DURATION_DATA_LINE_Y
+	clearIndex := 2
 	DIGIT_OFFSET := TIME_DIGIT_WIDTH + TIME_COLON_WIDTH
 	if change == MINUTE_CHANGED {
 		x += DIGIT_OFFSET
+		clearIndex = 3
 	}
 	if change == SECOND_CHANGED {
 		x += 2 * DIGIT_OFFSET
+		clearIndex = 4
 	}
-	d.printDigits(digits, DURATION_DATA_FONT, d.theme.DurationDataColor, x, y)
+	d.printDigits(digits, DURATION_DATA_FONT, d.theme.DurationDataColor, x, y, clearIndex)
 }
 
 func (d *dashboardDisplay) UpdateDisplay() {
 	d.display.Update()
 }
 
-func (d *dashboardDisplay) printDigits(digits string, font fonts.BitmapFont, color colors.Color, x, y int) {
+func (d *dashboardDisplay) printDigits(digits string, font fonts.BitmapFont, color colors.Color, x, y, clearIndex int) {
 	d.setTextSettings(font, color, x, y)
-	x1, y1, x2, y2 := d.display.GetTextArea(digits)
-	d.display.ClearArea(float64(x+x1), float64(y+y1), float64(x+x2), float64(y+y2))
+	d.clearArea(digits, x, y, clearIndex)
 	d.display.Write(digits)
 }
 
-func (d *dashboardDisplay) printDurationDigits(t int, change int) {
-	digits := fmt.Sprintf("%02d", t)
-	x := DATA_X
-	y := DURATION_DATA_LINE_Y
-	DIGIT_OFFSET := TIME_DIGIT_WIDTH + TIME_COLON_WIDTH
-	if change == MINUTE_CHANGED {
-		x += DIGIT_OFFSET
+func (d *dashboardDisplay) clearArea(text string, x, y, clearZoneIndex int) {
+	cz := d.clearZones[clearZoneIndex]
+	d.display.ClearArea(float64(x+cz.x1), float64(y+cz.y1), float64(x+cz.x2), float64(y+cz.y2))
+	x1, y1, x2, y2 := d.display.GetTextArea(text)
+	d.clearZones[clearZoneIndex] = clearZone{
+		x1: x1,
+		y1: y1,
+		x2: x2,
+		y2: y2,
 	}
-	if change == SECOND_CHANGED {
-		x += 2 * DIGIT_OFFSET
-	}
-	d.printDigits(digits, DURATION_DATA_FONT, d.theme.DurationDataColor, x, y)
 }
 
 func (d *dashboardDisplay) printDurationColons() {
--- ssh-run.sh
+++ ssh-run.sh
@@ -11,13 +11,13 @@ copy_codes(){
 # copy_codes ./ config.json
 # copy_codes ./speedometer types.go
 # copy_codes ./speedometer initialise.go
-copy_codes ./speedometer speedometer.go
+# copy_codes ./speedometer speedometer.go
 # copy_codes ./cmd/speedometer main.go
 # copy_codes ./dashboard info.go
 # copy_codes ./dashboard types.go
 # copy_codes ./dashboard constants.go
-# copy_codes ./dashboard dashboard.go
-# copy_codes ./dashboard background.go
+copy_codes ./dashboard dashboard.go
+copy_codes ./dashboard background.go
 # copy_codes ./dashboard theme.go
 
 ssh -t $raspizero "cd ~/go/src/speedometer-go; /usr/local/go/bin/go run ./cmd/speedometer"
