92eeb236b6254661bbc8e15cdc79c3a9db625181
marksaravi <mark.saravi@gmail.com>
2023-03-25 14:13:36 +1100

    feat: adding hardware init

--- display/display.go
+++ display/display.go
@@ -3,20 +3,29 @@ package display
 import (
 	"log"
 	"time"
+
+	"github.com/marksaravi/drawings-go/drawings"
+	"github.com/marksaravi/drivers-go/colors"
 )
 
 type display struct {
 	resetChannel chan bool
+	sketcher     drawings.Sketcher
 }
 
-func NewDisplay() *display {
+func NewDisplay(sketcher drawings.Sketcher) *display {
 	resetChannel := make(chan bool)
 	return &display{
 		resetChannel: resetChannel,
+		sketcher:     sketcher,
 	}
 }
 
-func (d *display) Initialize() {}
+func (d *display) Initialize() {
+	d.sketcher.Clear(colors.WHITE)
+	d.sketcher.FillCircle(160, 120, 50, colors.RED)
+	d.sketcher.Update()
+}
 
 func (d *display) Update(speed float64, distance float64, duration time.Duration) {}
 
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/marksaravi/speedometer-go
 go 1.20
 
 require (
+	github.com/marksaravi/drawings-go v1.0.1 // indirect
 	github.com/marksaravi/drivers-go v1.0.3 // indirect
+	github.com/marksaravi/fonts-go v0.4.0 // indirect
 	periph.io/x/conn/v3 v3.6.10 // indirect
 	periph.io/x/host/v3 v3.7.2 // indirect
 )
